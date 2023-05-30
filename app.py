16b356b2368f1a2d6265653cdec0da887cfd0f5b
Daniel DIas Pires <danddpp@gmail.com>
2023-05-29 23:37:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,51 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, dash_table
 import plotly.express as px
+import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 import pandas as pd
-#import geopandas as gpd
+import geopandas as gpd
+import json
 
 
-#gdf_bairros = gpd.read_file('bairros.geojson')
-#gdf_projecao_edificacao = gpd.read_file('projecao_edificacao.geojson')
+votacao_por_bairros_df = pd.read_csv('votacao_por_bairros.csv')
+
+#token = open(".mapbox_token").read()
+bairros_geojson = json.load(open("bairros_vencedor.geojson", "r"))
+
+bairros_geo = []
+for bairro in bairros_geojson['features']:
+#bairros_geojson["features"][i]["geometry"]["coordinates"])
+    bairros_geo.append({
+        'type': 'Feature',
+        'geometry': bairro['geometry'],
+        'id':bairro['properties']['nome_bairr']
+    })
+    #print(bairro['properties']['nome_bairr'])
+bairros_geo_ok = {'type': 'FeatureCollection', 'features': bairros_geo}
+
+bairros_csv = pd.read_csv("bairros.csv",  dtype={"nome_bairr": str})
+bairros_csv.insert(2, "QT_VOTOS_1º", [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], True)
+bairros_csv.insert(3, "QT_VOTOS_2º", [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], True)
 
 
 
-#retornando os centroids da camada projecao_edificacao ()
-#gdf_projecao_edificacao['geometry'] = gdf_projecao_edificacao[(gdf_projecao_edificacao['tp_constru'] != 'Garagem')|    (gdf_projecao_edificacao['tp_constru'] != 'Piscina')|(gdf_projecao_edificacao['tp_constru'] != 'Telheiro')|(gdf_projecao_edificacao['tp_constru'] != 'Ed�cula')].centroid
 
-#configurando o gráfico de atividades do município
-#df = gdf_projecao_edificacao.groupby('atividade_').agg('sum')
-#fig = px.bar(df, x=df.index, y=df['n_paviment'], color=df['n_paviment'], barmode="group")
+bairros_geojson2 = json.load(open("bairros_geo2.geojson", "r"))
+
+bairros_geo2 = []
+for bairro in bairros_geojson2['features']:
+#bairros_geojson["features"][i]["geometry"]["coordinates"])
+    bairros_geo2.append({
+        'type': 'Feature',
+        'geometry': bairro['geometry'],
+        'id':bairro['properties']['BAIRRO']
+    })
+    #print(bairro['properties']['nome_bairr'])
+bairros_geo_ok2 = {'type': 'FeatureCollection', 'features': bairros_geo2}
+
+
+
+
 df = pd.read_csv("votacao_2020.csv")
 
 bar = px.bar(df, x=df['Partido'], y=df['Votos_Validos'], 
@@ -38,58 +68,177 @@ options_list = ["2000 ou mais votos",
 
 df_secoes = pd.read_csv("votacao_2020_secoes.csv")
 list_names = df_secoes.groupby("NM_VOTAVEL").agg("sum")
-
-
-
+list_bairros = df_secoes.groupby("BAIRRO").agg("sum").index
 
 
 app = Dash(__name__)
 server = app.server
 
 app.layout = dbc.Container(children=[
-    html.H1("Estatísticas Eleições 2020", className="text-primary"),
-    dbc.Row([
-        html.H3("Vereadores - Votação por Partido", className="text-primary"), 
-        dbc.Col([
-            dcc.Graph(
-                figure=bar
-            ),         
-        ], xs=10, sm=8, md=5, lg=6, xl=5),
-    ]),
     html.Hr(),#quebra de linha
     dbc.Row([
-        dbc.Col([
-            html.H3("Votação por vereador - Intervalos", className="text-primary"),
-            dcc.Dropdown(
-                id="my-input",
-                options=options_list,
-                value="150 a 249 votos",
-                clearable=False,
-                style={"width":"40%"}
-            ),
-            html.Br(),  
-            dcc.Graph(id='my-output'),         
-        ], xs=10, sm=8, md=5, lg=6, xl=5)
-    ]),
-    
-    html.Hr(),
+           html.H1("Estatísticas Eleições Municipais - Caraguatatuba-SP 2020", className="text-primary", style={"text-align":"center"}),
+           dbc.Col([
+               dbc.Card([
+               html.H3("Vereadores - Votação por Partido", className="text-primary"),           
+               ], style={"height":"100%", "padding":"20px"})
+           ], width=4),
+
+           dbc.Col(
+               dbc.Card(dcc.Graph(figure=bar, style={"height":"100%", "padding":"10px"})), width=8),
+       ], style={"margin":"10px"}),
+
+
+    html.Hr(),#quebra de linha
     dbc.Row([
-        dbc.Col([
-            html.H3("Votação por região e bairro", className="text-primary"),
-            dcc.Dropdown(
-                id="my-input-regiao-bairro",
-                options=list_names.index,
-                value="ABEL GAMA",
-                clearable=False,
-                style={"width":"40%"}
-            ),
-            html.Div(children=[
-                dcc.Graph(id="my-output-regiao", style={'display': 'inline-block'}),
-                dcc.Graph(id="my-output-bairro", style={'display': 'inline-block'})
-            ]),
-        ], xs=10, sm=8, md=5, lg=6, xl=5)
+           dbc.Col([
+               dbc.Card([
+                   html.H3("Votação por vereador - Intervalos", className="text-primary"),
+                   html.Label("Intervalos"),
+                   html.Div(
+                       dcc.Dropdown(
+                           id="my-input",
+                           options=options_list,
+                           value="150 a 249 votos",
+                           clearable=False,
+                           style={"width":"40%"}
+                       ),
+                   ),
+               ], style={"height":"100%", "padding":"20px"})
+           ], width=4),
+
+           dbc.Col(
+               dbc.Card(dcc.Graph(id='my-output', style={"height":"100%", "padding":"10px"})), width=8),
+       ], style={"margin":"10px"}),
+    
+    html.Hr(),#quebra de linha
+    dbc.Row([
+           dbc.Col([
+               dbc.Card([
+                   html.H3("Distribuição de votos de um candidato por regiões e bairros", className="text-primary"),
+                   html.Label("Nome"),
+                   html.Div(
+                       dcc.Dropdown(
+                           id="my-input-regiao-bairro",
+                           options=list_names.index,
+                           value="ABEL GAMA",#["ABEL GAMA"],
+                           clearable=False,
+                           style={"width":"40%"},
+                           #multi=True
+                        ),
+                   ),
+               ], style={"height":"100%", "padding":"20px"})
+           ], width=4),
+
+           dbc.Col(
+               dbc.Card(
+                   html.Div(children=[
+                       dcc.Graph(id="my-output-regiao", style={'display': 'inline-block', "padding":"10px"}),
+                       dcc.Graph(id="my-output-bairro", style={'display': 'inline-block', "padding":"10px"})
+                   ]),
+               ), width=8),
+       ], style={"margin":"10px"}),
+
+    html.Hr(),#quebra de linha
+    dbc.Row([
+           dbc.Col([
+               dbc.Card([
+                   html.H3("10 + votados por cargo em um determinado bairro", className="text-primary"),
+                   html.Label("Cargo"),
+                   html.Div(
+                       dcc.Dropdown(
+                           id="my-input-cargo",
+                           options=["Prefeito","Vereador"],
+                           value="Prefeito",
+                           clearable=False,
+                           style={"width": "40%"},
+                           #persistence=True,
+                           #persistence_type="session",
+                           #multi=True#permite selecionar mais de uma opção simultaneamente
+                       ),
+                   ),
+
+                   html.Label("Bairro", style={"margin-top":"10px"}),
+                   html.Div(
+                       dcc.Dropdown(
+                           id="my-input-bairro",
+                           options=list_bairros,
+                           value=list_bairros[0],
+                           clearable=False,
+                           style={"width": "40%"},
+                           #persistence=True,
+                           #persistence_type="session",
+                           #multi=True#permite selecionar mais de uma opção simultaneamente
+                       ),
+                   ),
+               ], style={"height":"100%", "padding":"20px"})
+           ], width=4),
+
+           dbc.Col(
+               dbc.Card(dcc.Graph(id='my-output-cargo-bairro', style={"height":"100%", "padding":"10px"})), width=8),
+       ], style={"margin":"10px"}),
+
+       html.Hr(),#quebra de linha
+       dbc.Row([
+           html.H1("Candidato x Candidato", className="text-primary", style={"text-align":"center"}),
+           html.Div([
+                   html.Label("Candidato 1"),
+                   html.Div(
+                       dcc.Dropdown(
+                           id="input-candidato-1",
+                           options=list_names.index,
+                           value="ABEL GAMA",
+                           clearable=False,
+                           style={"width": "40%"},
+                           #persistence=True,
+                           #persistence_type="session",
+                           #multi=True#permite selecionar mais de uma opção simultaneamente
+                       ),
+                   ),
+
+                   html.Label("Candidato 2", style={"margin-top":"10px"}),
+                   html.Div(
+                       dcc.Dropdown(
+                           id="input-candidato-2",
+                           options=list_names.index,
+                           value="AGUINALDO BROW",
+                           clearable=False,
+                           style={"width": "40%"},
+                           #persistence=True,
+                           #persistence_type="session",
+                           #multi=True#permite selecionar mais de uma opção simultaneamente
+                       ),
+                   ),
+       ]),
+           dbc.Col(
+               dbc.Card(dcc.Graph(id="chloropleth-map", style={"height":"100%", "padding":"10px"})), width=8),
+       ], style={"margin":"10px"}),
+
+
+       html.Hr(),#quebra de linha
+       dbc.Row([
+           html.H1("Candidato", className="text-primary", style={"text-align":"center"}),
+           html.Div([
+                   html.Label("Candidato"),
+                   html.Div(
+                       dcc.Dropdown(
+                           id="input-candidato",
+                           options=list_names.index,
+                           value="ABEL GAMA",
+                           clearable=False,
+                           style={"width": "40%"},
+                           #persistence=True,
+                           #persistence_type="session",
+                           #multi=True#permite selecionar mais de uma opção simultaneamente
+                       ),
+                   ),
+       ]),
+           dbc.Col(
+               dbc.Card(dcc.Graph(id="chloropleth-map2", style={"height":"100%", "padding":"10px"})), width=8),
+       ], style={"margin":"10px"}),
+
     ])
-], fluid=True)
+
 
 #callbacks
 @app.callback(
@@ -126,10 +275,12 @@ def update_figure(option):
     Output('my-output-regiao', 'figure'),
     Input('my-input-regiao-bairro', 'value'))
 def update_figure(option):
+    
     op = df_secoes[df_secoes["NM_VOTAVEL"] == option]
+    #op = df_secoes[df_secoes["NM_VOTAVEL"].isin(option)]
     candidato = op.groupby("REGIAO").agg("sum")
-
-    fig = px.bar(candidato, x=candidato.index, y='QT_VOTOS', color=candidato.index)
+    
+    fig = px.bar(candidato, x=candidato.index, y='QT_VOTOS', color=candidato.index, barmode="group")
     fig.update_layout(
         transition_duration=500,
         autosize=False,
@@ -145,19 +296,137 @@ def update_figure(option):
     Input('my-input-regiao-bairro', 'value'))
 def update_figure(option):
     op = df_secoes[df_secoes["NM_VOTAVEL"] == option]
+    #op = df_secoes[df_secoes["NM_VOTAVEL"].isin(option)]
     candidato = op.groupby("BAIRRO").agg("sum")
-
+    
     fig = px.bar(candidato, x=candidato.index, y='QT_VOTOS', color=candidato.index)
     fig.update_layout(
         transition_duration=500,
         autosize=False,
         width=900,
-        height=600
+        height=600,
     )
   
     return fig
 
 
+@app.callback(
+    Output('my-output-cargo-bairro', 'figure'),
+    Input('my-input-cargo', 'value'),
+    Input('my-input-bairro', 'value'))
+def update_figure(cargo, bairro):
+    df_bairro = df_secoes[(df_secoes["BAIRRO"] == bairro) & (df_secoes["DS_CARGO_PERGUNTA"] == cargo)]
+    df_bairro_agrup = df_bairro.groupby("NM_VOTAVEL").agg("sum")
+    df_output = df_bairro_agrup.sort_values('QT_VOTOS', ascending=False).head(10)
+
+    fig = px.bar(df_output, x=df_output.index, y='QT_VOTOS', orientation="v")
+    fig.update_layout(
+        transition_duration=500,
+        autosize=False,
+        #width=1200,
+        #height=900
+    )
+  
+    return fig
+
+
+
+@app.callback(
+    Output('chloropleth-map', 'figure'),
+    Input('input-candidato-1', 'value'),
+    Input('input-candidato-2', 'value')
+)
+def update_chloroleth_map(c1, c2):
+    candidato1 = df_secoes[df_secoes['NM_VOTAVEL'] == c1]
+    votos_bairro_c1 = candidato1.groupby("BAIRRO").agg("sum")
+    votos_bairro_c1 = votos_bairro_c1.sort_values('BAIRRO', ascending=True)
+
+    candidato2 = df_secoes[df_secoes['NM_VOTAVEL'] == c2]
+    votos_bairro_c2 = candidato2.groupby("BAIRRO").agg("sum")
+    votos_bairro_c2 = votos_bairro_c2.sort_values('BAIRRO', ascending=True)
+
+    bairros_csv.sort_values('nome_bairr', ascending=True)
+     
+
+    for b in range(len(bairros_csv)):
+        qt_votos_c1 = 0
+        qt_votos_c2 = 0
+        for i in range(len(votos_bairro_c1)):
+            #print(bairros_csv['nome_bairr'][b])
+            if votos_bairro_c1.index[i] == bairros_csv['nome_bairr'][b]:
+                qt_votos_c1 = votos_bairro_c1['QT_VOTOS'][i]
+        for j in range(len(votos_bairro_c2)):
+            if votos_bairro_c2.index[j] == bairros_csv['nome_bairr'][b]:
+                qt_votos_c2 = votos_bairro_c2['QT_VOTOS'][j]
+        if qt_votos_c1 > qt_votos_c2:
+            bairros_csv['Vencedor'][b] = c1
+            bairros_csv['QT_VOTOS_1º'][b] = qt_votos_c1
+            bairros_csv['QT_VOTOS_2º'][b] = qt_votos_c2 
+        if qt_votos_c2 > qt_votos_c1:
+            bairros_csv['Vencedor'][b] = c2
+            bairros_csv['QT_VOTOS_1º'][b] = qt_votos_c2
+            bairros_csv['QT_VOTOS_2º'][b] = qt_votos_c1 
+        if (qt_votos_c1 == qt_votos_c2) and (qt_votos_c1 > 0) and (qt_votos_c2 > 0):
+            bairros_csv['Vencedor'][b] = 'Empate'
+            bairros_csv['QT_VOTOS_1º'][b] = qt_votos_c1
+            bairros_csv['QT_VOTOS_2º'][b] = qt_votos_c2
+        if (qt_votos_c1 == 0) and (qt_votos_c2 == 0):
+            bairros_csv['Vencedor'][b] = 'Sem votos'
+            bairros_csv['QT_VOTOS_1º'][b] = 0
+            bairros_csv['QT_VOTOS_2º'][b] = 0 
+    
+    #print(bairros_csv)
+
+
+    fig = px.choropleth_mapbox(
+        data_frame=bairros_csv,#dataframe com os dados 
+        geojson=bairros_geo_ok,#o arquivo de feições já com a coluna id que vincula à coluna do dataframe 
+        locations='nome_bairr',#atributo que vincula o dataframe ao geojson
+        color=bairros_csv["Vencedor"],#a coluna do dataframe que irá colorir o mapa
+        mapbox_style="open-street-map",
+        center={"lat": -23.6674133,"lon":-45.4406341},
+        zoom=10,
+        hover_data={'nome_bairr': True, 'Vencedor': True,  'QT_VOTOS_1º': True, 'QT_VOTOS_2º': True},
+        opacity=0.5,
+        title="Teste"
+    )
+
+    fig.update_layout(
+        margin={'r':5,'t':5,'l':5,'b':5},
+    )
+    return fig
+
+
+
+
+
+@app.callback(
+    Output('chloropleth-map2', 'figure'),
+    Input('input-candidato', 'value'),
+)
+def update_chloroleth_map(c1):
+    candidato1 = df_secoes[df_secoes['NM_VOTAVEL'] == c1]
+    votos_bairro = candidato1.groupby("BAIRRO").agg("sum")
+    votos_bairro['BAIRRO'] = votos_bairro.index 
+    print(votos_bairro)
+    #df_out = bairros_geo_ok.rename(columns={'BAIRRO':'nome_bairr'})
+    fig = px.choropleth_mapbox(
+        data_frame=votos_bairro,#dataframe com os dados 
+        geojson=bairros_geo_ok2,#o arquivo de feições já com a coluna id que vincula à coluna do dataframe 
+        locations='BAIRRO',#atributo que vincula o dataframe ao geojson
+        color=votos_bairro["QT_VOTOS"],#a coluna do dataframe que irá colorir o mapa
+        mapbox_style="open-street-map",
+        center={"lat": -23.6674133,"lon":-45.4406341},
+        zoom=10,
+        hover_data={'BAIRRO': True, 'QT_VOTOS': True},
+        opacity=0.5,
+        title="Teste"
+    )
+
+    fig.update_layout(
+        margin={'r':5,'t':5,'l':5,'b':5},
+    )
+    return fig
 
 
 
